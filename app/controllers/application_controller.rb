e1acc70090620fb18c129d9e5c6079d6d1406a8c
Houndjetodé Noukpo Hervé <vetcho006@hotmail.com>
2022-06-16 20:08:27 +0100

    first version rails 7 + hotwire entirely

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  # Add this line to see the progress bar long enough
+  # and remove it when it has the expected styles
+  # before_action -> { sleep 3 }
+  
+  before_action :authenticate_user!, unless: :devise_controller?
+  helper_method :current_company
+  private
+
+  def current_company
+    @current_company ||= current_user.company if user_signed_in?
+  end
+    
+end
---
+++ app/models/quote.rb
@@ -0,0 +1,14 @@
+class Quote < ApplicationRecord
+  belongs_to :company
+
+
+  validates :name, presence: true
+
+  scope :ordered, -> { order(id: :desc) }
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+  # broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+end
